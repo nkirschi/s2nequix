2d2f1e502a8c0b59ac451a78477694c70509dc8a
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2026-01-14 15:53:09 +0100

    Fix matbench download link

--- data/download_mptrj.sh
+++ data/download_mptrj.sh
@@ -3,7 +3,7 @@
 # NOTE: this wget is blocked by figshare, so might need to download manually
 # 2023-11-22-mp-trj-extxyz-by-yuan.zip
 if [ ! -f "data/2023-11-22-mp-trj-extxyz-by-yuan.zip" ]; then
-    wget --content-disposition https://figshare.com/files/43302033 -P data/
+    wget --no-check-certificate --content-disposition https://ndownloader.figshare.com/files/43302033 -P data/
 fi
 
 # unzip data/2023-11-22-mp-trj-extxyz-by-yuan.zip
